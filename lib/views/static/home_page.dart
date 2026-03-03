1085f1929c04046a7a76059f5ae2fdbf4c2a9897
Your Name <you@example.com>
2026-03-03 17:22:27 +0000

    assurance1

--- lib/views/static/home_page.dart
+++ lib/views/static/home_page.dart
@@ -6,7 +6,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: const Color.fromARGB(255, 245, 244, 244),
+
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const ListTile(
@@ -18,20 +19,18 @@ class HomePage extends StatelessWidget {
         ),
         actions: [
           CircleAvatar(
-            backgroundColor: const Color.fromARGB(255, 234, 243, 255),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notifications_active_outlined, size: 25),
-            ),
+            backgroundColor: Color.fromARGB(255, 234, 243, 255),
+            child: Icon(Icons.notifications_active_outlined, size: 25),
           ),
-          const SizedBox(width: 20),
-          const CircleAvatar(
+          SizedBox(width: 20),
+          CircleAvatar(
             radius: 25,
             backgroundImage: AssetImage("asset/mariam.jpeg"),
           ),
-          const SizedBox(width: 10),
+          SizedBox(width: 10),
         ],
       ),
+
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: ListView(
@@ -40,7 +39,7 @@ class HomePage extends StatelessWidget {
               height: 220,
               padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 51, 48, 223),
+                color: Color.fromARGB(255, 51, 48, 223),
                 borderRadius: BorderRadius.circular(20),
               ),
               child: Column(
@@ -49,12 +48,12 @@ class HomePage extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
+                      Text(
                         "Cotisation mensuelle",
                         style: TextStyle(color: Colors.white70, fontSize: 16),
                       ),
                       Container(
-                        padding: const EdgeInsets.symmetric(
+                        padding: EdgeInsets.symmetric(
                           horizontal: 15,
                           vertical: 6,
                         ),
@@ -62,7 +61,7 @@ class HomePage extends StatelessWidget {
                           color: Colors.white24,
                           borderRadius: BorderRadius.circular(20),
                         ),
-                        child: const Text(
+                        child: Text(
                           "À jour",
                           style: TextStyle(
                             color: Colors.white,
@@ -72,30 +71,26 @@ class HomePage extends StatelessWidget {
                       ),
                     ],
                   ),
-
-                  const SizedBox(height: 15),
-
-                  const Text(
-                    "934.7387 FCFA",
+                  SizedBox(height: 15),
+                  Text(
+                    "9.347.387 FCFA",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-
-                  const SizedBox(height: 25),
-
+                  SizedBox(height: 25),
                   Row(
                     children: [
                       Expanded(
                         child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          padding: EdgeInsets.symmetric(vertical: 15),
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(15),
                           ),
-                          child: const Center(
+                          child: Center(
                             child: Text(
                               "Mes paiements",
                               style: TextStyle(
@@ -106,15 +101,15 @@ class HomePage extends StatelessWidget {
                           ),
                         ),
                       ),
-                      const SizedBox(width: 15),
+                      SizedBox(width: 15),
                       Expanded(
                         child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          padding: EdgeInsets.symmetric(vertical: 15),
                           decoration: BoxDecoration(
                             color: Colors.white24,
                             borderRadius: BorderRadius.circular(15),
                           ),
-                          child: const Center(
+                          child: Center(
                             child: Text(
                               "Support",
                               style: TextStyle(
@@ -129,6 +124,307 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+
+            SizedBox(height: 25),
+
+            Text(
+              "Actions Rapides",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 15),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEAF3FF),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Icon(
+                        Icons.note_add_outlined,
+                        color: Color.fromARGB(255, 51, 48, 223),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text("Déclarer\nsinistre", textAlign: TextAlign.center),
+                  ],
+                ),
+
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEAF3FF),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Icon(
+                        Icons.description_outlined,
+                        color: Color.fromARGB(255, 51, 48, 223),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text("Mes\ndocuments", textAlign: TextAlign.center),
+                  ],
+                ),
+
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEAF3FF),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Icon(
+                        Icons.phone_outlined,
+                        color: Color.fromARGB(255, 51, 48, 223),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text("Urgence\n24/7", textAlign: TextAlign.center),
+                  ],
+                ),
+
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEAF3FF),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Icon(
+                        Icons.settings_outlined,
+                        color: Color.fromARGB(255, 51, 48, 223),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text("Gérer\noptions", textAlign: TextAlign.center),
+                  ],
+                ),
+              ],
+            ),
+
+            SizedBox(height: 25),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Mes Assurances",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Tout voir",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 51, 48, 223),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+
+            Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFDCEBFA),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Icon(Icons.directions_car, color: Colors.blue),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Auto • Toyota Yaris",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "N° 4829103 • Tous risques",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade100,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text("Actif", style: TextStyle(color: Colors.green)),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFDDF2E3),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Icon(Icons.home_outlined, color: Colors.green),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Habitation • Principale",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "24 Rue de la Paix, Abidjan",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade100,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text("Actif", style: TextStyle(color: Colors.green)),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF8DADA),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Icon(Icons.favorite_border, color: Colors.red),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Santé • Famille",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "Formule Confort Plus",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade100,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text("Actif", style: TextStyle(color: Colors.green)),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 15),
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.grid_view, color: Colors.blue),
+                SizedBox(height: 5),
+                Text("Accueil", style: TextStyle(color: Colors.blue)),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.description_outlined, color: Colors.grey),
+                SizedBox(height: 5),
+                Text("Contrats", style: TextStyle(color: Colors.grey)),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.shield_outlined, color: Colors.grey),
+                SizedBox(height: 5),
+                Text("Sinistres", style: TextStyle(color: Colors.grey)),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.person_outline, color: Colors.grey),
+                SizedBox(height: 5),
+                Text("Profil", style: TextStyle(color: Colors.grey)),
+              ],
             ),
           ],
         ),
